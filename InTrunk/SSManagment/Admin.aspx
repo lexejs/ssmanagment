80d62229dec8d5416af4047f0a0bf3707e30e285
lexejs <lexejs@466f9f44-f6f4-11dd-9fdb-3debd5008f28>
2009-02-12 16:57:10 +0000

    odminko: group management

--- InTrunk/SSManagment/Admin.aspx
+++ InTrunk/SSManagment/Admin.aspx
@@ -2,19 +2,43 @@
 	AutoEventWireup="true" CodeBehind="Admin.aspx.cs" Inherits="SSManagment.Admin" %>
 
 <asp:Content ID="Content1" ContentPlaceHolderID="cphAdminMenu" runat="server">
+
 </asp:Content>
+
 <asp:Content ID="Content2" ContentPlaceHolderID="cphAdminObject" runat="server">
 	<div>
-		<span>
-			<asp:ListBox ID="lstGroup" runat="server"></asp:ListBox>
-			<asp:TextBox ID="txtGroupName" runat="server"></asp:TextBox>
-			<asp:Button ID="btnAddGroup" runat="server" Text="Добавить" />
-			<asp:Button ID="btnDelGroup" runat="server" Text="Удалить" />
-		</span><span>
-			<asp:ListBox ID="lstSubGroup" runat="server"></asp:ListBox>
-			<asp:TextBox ID="txtSubGroupName" runat="server"></asp:TextBox>
-			<asp:Button ID="btnAddSubGroup" runat="server" Text="Добавить" />
-			<asp:Button ID="btnDellSubGroup" runat="server" Text="Удалить" />
-		</span>
+		<table>
+			<tr>
+				<td>
+					<asp:ListBox ID="lstGroup" runat="server" Height="300px" Width="300px" OnSelectedIndexChanged="lstGroup_SelectedIndexChanged"
+						AutoPostBack="True"></asp:ListBox>
+				</td>
+			</tr>
+			<tr>
+				<td>
+					<asp:TextBox ID="txtGroupName" runat="server"></asp:TextBox>
+					<asp:Button ID="btnAddGroup" runat="server" Text="Добавить" 
+						onclick="btnAddGroup_Click" />
+					<asp:Button ID="btnDelGroup" runat="server" Text="Удалить" 
+						onclick="btnDelGroup_Click" />
+				</td>
+			</tr>
+		</table>
+		<table>
+			<tr>
+				<td>
+					<asp:ListBox ID="lstSubGroup" runat="server" Height="300px" Width="300px" AutoPostBack="true">
+					</asp:ListBox>
+				</td>
+			</tr>
+			<tr>
+				<td>
+					<asp:TextBox ID="txtSubGroupName" runat="server"></asp:TextBox>
+					<asp:Button ID="btnAddSubGroup" runat="server" Text="Добавить" 
+						onclick="btnAddSubGroup_Click" />
+					<asp:Button ID="btnDellSubGroup" runat="server" Text="Удалить" />
+				</td>
+			</tr>
+		</table>
 	</div>
 </asp:Content>
--- InTrunk/SSManagment/Admin.aspx.cs
+++ InTrunk/SSManagment/Admin.aspx.cs
@@ -3,15 +3,86 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
-using System.Web.UI.WebControls;
+using SSManagment.Models;
 
 namespace SSManagment
 {
 	public partial class Admin : System.Web.UI.Page
 	{
+		IList<group> groups;
 		protected void Page_Load(object sender, EventArgs e)
+		{
+			var db = new ssmDataContext();
+			groups = db.groups.ToList();
+
+			if (!Page.IsPostBack)
+			{
+				lstGroupFill();
+			}
+		}
+
+		private void lstGroupFill()
+		{
+			var db = new ssmDataContext();
+
+			groups = db.groups.ToList();
+			lstGroup.DataSource = groups.Where(g => g.parent == null).ToList();
+			lstGroup.DataTextField = "name";
+			lstGroup.DataValueField = "id";
+			lstGroup.DataBind();
+			txtGroupName.Text = "";
+		}
+
+		protected void lstGroup_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			var parentId = int.Parse(lstGroup.SelectedItem.Value);
+			lstSubGroup.DataSource = groups.Where(g => g.parent != null && g.parent.Value == parentId).ToList();
+			lstSubGroup.DataTextField = "name";
+			lstSubGroup.DataValueField = "id";
+			lstSubGroup.DataBind();
+			txtGroupName.Text = lstGroup.SelectedItem.Text;
+		}
+
+		protected void lstSubGroup_SelectedIndexChanged(object sender, EventArgs e)
 		{
 
 		}
+
+		protected void btnAddGroup_Click(object sender, EventArgs e)
+		{
+			var db = new ssmDataContext();
+			var nGroup = new group() { name = txtGroupName.Text };
+			db.groups.InsertOnSubmit(nGroup);
+			db.SubmitChanges();
+			lstGroupFill();
+		}
+
+		protected void btnDelGroup_Click(object sender, EventArgs e)
+		{
+			var db = new ssmDataContext();
+			var groupId = int.Parse(lstGroup.SelectedItem.Value);
+			var groupToDelete = db.groups.Where(g => g.id == groupId && g.groups.Count == 0).ToList();
+			if (groupToDelete.Count == 1)
+			{
+				db.groups.DeleteOnSubmit(groupToDelete.First());
+				db.SubmitChanges();
+				lstGroupFill();
+			}
+		}
+
+		protected void btnAddSubGroup_Click(object sender, EventArgs e)
+		{
+			var db = new ssmDataContext();
+			if (lstGroup.SelectedIndex > -1)
+			{
+				var parentId = int.Parse(lstGroup.SelectedItem.Value);
+        var nGroup = new group() { name = txtSubGroupName.Text, parent = parentId };
+				db.groups.InsertOnSubmit(nGroup);
+				db.SubmitChanges();
+				lstGroupFill();
+
+			}
+		}
+
 	}
 }
