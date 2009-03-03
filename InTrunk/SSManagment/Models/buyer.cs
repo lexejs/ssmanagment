f0e2c683ad4c853ab86e32555b42baf15ce818b7
lexejs <lexejs@466f9f44-f6f4-11dd-9fdb-3debd5008f28>
2009-03-03 15:33:41 +0000

    run into debt

---
+++ InTrunk/SSManagment/Models/buyer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SSManagment.Models
+{
+	public partial class buyer
+	{
+		public void RunIntoDebt(int buyerId, float cash)
+		{
+			var db = new ssmDataContext();
+			buyer buyer = db.buyers.First(b => b.id == buyerId);
+			if (buyer.debt != null)
+			{
+				buyer.debt += cash;
+			}
+			else
+			{
+				buyer.debt = cash;
+			}
+			db.SubmitChanges();
+		}
+
+
+
+	}
+}
