b769f3c881b7dcdd2ff8b8e33a3be7c15eb55730
lexejs <lexejs@466f9f44-f6f4-11dd-9fdb-3debd5008f28>
2010-03-15 16:18:05 +0000

    отчтёты

--- InTrunk/SSManagment/ViewReport.aspx.designer.cs
+++ InTrunk/SSManagment/ViewReport.aspx.designer.cs
@@ -86,6 +86,15 @@ namespace SSManagment {
         protected global::System.Web.UI.WebControls.Button btnShow;
         
         /// <summary>
+        /// lblMessage control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblMessage;
+        
+        /// <summary>
         /// reportViewer1 control.
         /// </summary>
         /// <remarks>
